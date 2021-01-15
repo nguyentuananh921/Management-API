0e436fb7cc5d2351818a08afb823011a0391b0e8
nguyentuananh921 <nguyentuananh921@gmail.com>
2021-01-15 16:59:05 +0700

    Restore DB and Then Backup + Create Controller

---
+++ WebAPI/Controllers/CarDriversController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarDriversController : ControllerBase
+    {
+        private readonly WebAPIContext _context;
+
+        public CarDriversController(WebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarDrivers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CarDriver>>> GetCarDrivers()
+        {
+            return await _context.CarDrivers.ToListAsync();
+        }
+
+        // GET: api/CarDrivers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarDriver>> GetCarDriver(int id)
+        {
+            var carDriver = await _context.CarDrivers.FindAsync(id);
+
+            if (carDriver == null)
+            {
+                return NotFound();
+            }
+
+            return carDriver;
+        }
+
+        // PUT: api/CarDrivers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarDriver(int id, CarDriver carDriver)
+        {
+            if (id != carDriver.PeopleID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carDriver).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarDriverExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CarDrivers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CarDriver>> PostCarDriver(CarDriver carDriver)
+        {
+            _context.CarDrivers.Add(carDriver);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CarDriverExists(carDriver.PeopleID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCarDriver", new { id = carDriver.PeopleID }, carDriver);
+        }
+
+        // DELETE: api/CarDrivers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCarDriver(int id)
+        {
+            var carDriver = await _context.CarDrivers.FindAsync(id);
+            if (carDriver == null)
+            {
+                return NotFound();
+            }
+
+            _context.CarDrivers.Remove(carDriver);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarDriverExists(int id)
+        {
+            return _context.CarDrivers.Any(e => e.PeopleID == id);
+        }
+    }
+}
---
+++ WebAPI/Controllers/CarInspectionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarInspectionsController : ControllerBase
+    {
+        private readonly WebAPIContext _context;
+
+        public CarInspectionsController(WebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarInspections
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CarInspection>>> GetCarInspections()
+        {
+            return await _context.CarInspections.ToListAsync();
+        }
+
+        // GET: api/CarInspections/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarInspection>> GetCarInspection(int id)
+        {
+            var carInspection = await _context.CarInspections.FindAsync(id);
+
+            if (carInspection == null)
+            {
+                return NotFound();
+            }
+
+            return carInspection;
+        }
+
+        // PUT: api/CarInspections/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarInspection(int id, CarInspection carInspection)
+        {
+            if (id != carInspection.CarInspectionID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carInspection).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarInspectionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CarInspections
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CarInspection>> PostCarInspection(CarInspection carInspection)
+        {
+            _context.CarInspections.Add(carInspection);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCarInspection", new { id = carInspection.CarInspectionID }, carInspection);
+        }
+
+        // DELETE: api/CarInspections/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCarInspection(int id)
+        {
+            var carInspection = await _context.CarInspections.FindAsync(id);
+            if (carInspection == null)
+            {
+                return NotFound();
+            }
+
+            _context.CarInspections.Remove(carInspection);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarInspectionExists(int id)
+        {
+            return _context.CarInspections.Any(e => e.CarInspectionID == id);
+        }
+    }
+}
